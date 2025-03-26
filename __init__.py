6396440a356f245c77c3e4defa12853a0a169cf4
Vladan Trhlík <vladantrhlik@gmail.com>
2025-03-26 09:01:39 +0100

    added ctrl+G shortcut to bring up pie menu

--- __init__.py
+++ __init__.py
@@ -3,11 +3,22 @@ from .operators import *
 from .menu import *
 
 ops = [GIHideOperator, GIMergeOperator, GIFindOperator, GIMenu, GIOpenMenu]
+addon_keymaps = []
 
 def register():
     for c in ops:
         bpy.utils.register_class(c)
+    
+    # assign Ctrl+G shortcut
+    wm = bpy.context.window_manager
+    kc = wm.keyconfigs.addon
+    if kc:
+        km = wm.keyconfigs.addon.keymaps.new(name='Node Editor', space_type='NODE_EDITOR')
+        kmi = km.keymap_items.new(GIOpenMenu.bl_idname, type='G', value='PRESS', ctrl=True)
+        addon_keymaps.append((km, kmi))
 
 def unregister():
     for c in ops:
         bpy.utils.unregister_class(c)
+    
+    addon_keymaps.clear()
